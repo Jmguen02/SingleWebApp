7e5703fdb351dfb1e672e02508783d2843ddf9c8
Joe Guenther <jmguen02@gmail.com>
2022-11-06 16:38:55 -0500

    first commit

---
+++ Single Web App Assingnment - Joe Guenther/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Single_Web_App_Assingnment___Joe_Guenther.Controllers.Models;
+
+namespace Single_Web_App_Assingnment___Joe_Guenther.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
